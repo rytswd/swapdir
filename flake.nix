3977d2711bafc83e09d7edf4be0524a3decca92e
Ryota <rytswd@gmail.com>
2026-06-15 15:04:48 +0100

    Add binary cache with cachix

--- flake.nix
+++ flake.nix
@@ -1,6 +1,13 @@
 {
   description = "swapdir - cross-shell directory path swapping utility";
 
+  nixConfig = {
+    extra-substituters = [ "https://swapdir.cachix.org" ];
+    extra-trusted-public-keys = [
+      "swapdir.cachix.org-1:AxK+CyOlKSBbZ/O2HhFz4V++zaIP1UqPaRenIbbFpUo="
+    ];
+  };
+
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs?ref=nixos-unstable";
     blueprint.url = "github:numtide/blueprint";
